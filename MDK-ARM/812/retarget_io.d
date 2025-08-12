2deacb0178191742a32c1695169c4507be83dae3
zhc-ww <zhc060617@qq.com>
2025-08-12 20:03:09 +0800

    8.12作业

---
+++ MDK-ARM/812/retarget_io.d
@@ -0,0 +1,4 @@
+812/retarget_io.o: \
+  E:\keil\ARM\CMSIS-Compiler\2.1.0\source\armcc\retarget_io.c \
+  RTE\_812\RTE_Components.h \
+  E:\keil\ARM\CMSIS-Compiler\2.1.0\include\retarget_stdout.h
---
+++ MDK-ARM/812/stdout_evr.d
@@ -0,0 +1,3 @@
+812/stdout_evr.o: E:\keil\ARM\CMSIS-Compiler\2.1.0\source\stdout_evr.c \
+  E:\keil\ARM\CMSIS-Compiler\2.1.0\include\retarget_stdout.h \
+  E:\keil\ARM\CMSIS-View\1.2.0\EventRecorder\Include\EventRecorder.h
